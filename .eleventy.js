32abf2fefe1a5299c2260e78eeefdd6732f1c873
alvinashiatey <alvinashiatey@gmail.com>
2020-04-02 16:22:35 -0400

    Blog post setup
    Blog setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,24 @@
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPassthroughCopy("assets");
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
   return {
     dir: {
       input: "src",
       output: "dist",
       data: "_data"
-    }
+    },
+    dataTemplateEngine: "njk"
   };
 };
