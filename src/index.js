4167bd8eab7ac48c6453e310944b423b543ad272
alvinashiatey <alvinashiatey@gmail.com>
2020-04-03 19:29:31 -0400

    update
    changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./assets/style/main.scss";
+import axios from "axios";
 import gsap from "gsap";
 import * as p5 from "p5";
 import "/Users/alvinkwabena/Documents/GitHub/kprize/node_modules/normalize.css/normalize.css";
@@ -83,3 +84,193 @@ let s = sk => {
 };
 
 const P5 = new p5(s);
+
+//google maps
+var map;
+function initMap() {
+  let myLatLng = { lat: 5.59322, lng: -0.23054 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: myLatLng,
+    zoom: 17,
+    disableDefaultUI: true,
+    styles: [
+      {
+        featureType: "all",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            weight: "2.00"
+          }
+        ]
+      },
+      {
+        featureType: "all",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#9c9c9c"
+          }
+        ]
+      },
+      {
+        featureType: "all",
+        elementType: "labels.text",
+        stylers: [
+          {
+            visibility: "on"
+          }
+        ]
+      },
+      {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#f2f2f2"
+          }
+        ]
+      },
+      {
+        featureType: "landscape",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      },
+      {
+        featureType: "landscape.man_made",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100
+          },
+          {
+            lightness: 45
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#eeeeee"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#7b7b7b"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#46bcec"
+          },
+          {
+            visibility: "on"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#c8d7d4"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#070707"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#ffffff"
+          }
+        ]
+      }
+    ]
+  });
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "Kuenyehia Prize for Contemporary Art"
+  });
+}
+const mapDOM = document.getElementById("map");
+if (mapDOM) {
+  initMap();
+}
